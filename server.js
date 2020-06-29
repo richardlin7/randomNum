04794da069a5ad69531af4f414591758d854259e
Richard Lin <rlin7@student.gsu.edu>
2020-06-29 18:50:08 -0500

    accepts all origins

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 const app = express();
 const rand = require("./random.js");
-
-// app.use(cors());
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   next();
-// });
 
 app.get("/", function (req, res) {
   res.header(
     "Access-Control-Allow-Origin",
-    //"http://localhost:8080/"
-    "http://richard.cheung.nyc:8080/"
+    "*"
+    //"http://localhost:8080"
+    //"http://richard.cheung.nyc:8080/"
   );
-  // res.setHeader(
-  //   "Access-Control-Allow-Methods",
-  //   "GET"
-  //   // "http://localhost:8080/"
-  //   // "http://richard.cheung.nyc:8080/"
-  // );
   res.send(rand.random().toString());
 });
 
