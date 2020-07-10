7c7465745cebcb88816376ce2c8a0d663d7ef8fd
Richard Lin <rlin7@student.gsu.edu>
2020-07-09 19:42:42 -0500

    added dependency

--- server.js
+++ server.js
@@ -1,58 +1,45 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const bodyParser = require("body-parser");
+const assert = require("assert");
 const rand = require("./random.js");
-// const mongoose = require("mongoose");
 
 // MongoDB code
 const MongoClient = require("mongodb").MongoClient;
-const url =
-  "mongodb+srv://richard:rlin9774@cluster0.vzfde.gcp.mongodb.net/random?retryWrites=true&w=majority";
-
-// const client = new MongoClient(url, { for mongoose
-//   useUnifiedTopology: true,
-//   useNewUrlParser: true,
-// });
-
-// MongoClient.set("useUnifiedTopology", true);
-
-// MongoClient.connect(url, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+// const url =
+//   "mongodb+srv://richard:rlin9774@cluster0.vzfde.gcp.mongodb.net/random?retryWrites=true&w=majority";
+const url = "mongodb://localhost:27017";
 
 // BodyParser code
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var corsOptions = {
+  origin: "http://localhost:8080",
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 // Requests code
+app.options("/", cors(corsOptions));
 app.post("/", function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  var high = parseInt(req.body.high, 10);
-  var low = parseInt(req.body.low, 10);
+  // res.header("Access-Control-Allow-Origin", "http://localhost:8080/");
+  // res.header("Access-Control-Allow-Methods", "POST");
+  // res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  var high = parseInt(req.body.high, 10); // converts strings to ints
+  var low = parseInt(req.body.low, 10); // converts strings to ints
   var id = req.connection.remoteAddress; // gets ip
 
-  if (high != parseInt(high, 10) || low != parseInt(low, 10)) {
-    // check if it is an int
-    MongoClient.connect(url, function (err, db) {
-      if (err) throw err; // if error occurs
-      var dbo = db.db("invalid-input"); // connects to database 'invalid-input'
-
-      var d = new Date(); // gets current date
-      var myobj = { high: high, low: low, ip: id, date: d };
-
-      dbo.collection("invalid-input1").insertOne(myobj, function (err, res) {
-        if (err) throw err;
-        console.log("Invalid Input: logged for inspection");
-        db.close();
-      });
-    });
-    return res.end();
-  }
   if (isNaN(high) || isNaN(low)) {
     // check if its a number
     MongoClient.connect(url, function (err, db) {
+      assert.equal(null, err);
       if (err) throw err; // if error occurs
+      console.log("Connected successfully to server");
       var dbo = db.db("invalid-input"); // connects to database 'invalid-input'
 
       var d = new Date(); // gets current date
@@ -69,7 +56,9 @@ app.post("/", function (req, res) {
   if (high <= 0 || low <= 0 || low >= high) {
     // check if low is greater than high, make sure they are not negative
     MongoClient.connect(url, function (err, db) {
+      assert.equal(null, err);
       if (err) throw err; // if error occurs
+      console.log("Connected successfully to server");
       var dbo = db.db("invalid-input"); // connects to database 'invalid-input'
 
       var d = new Date(); // gets current date
@@ -77,7 +66,7 @@ app.post("/", function (req, res) {
 
       dbo.collection("invalid-input1").insertOne(myobj, function (err, res) {
         if (err) throw err;
-        console.log("Invalid Input: Logged for Inspection");
+        console.log("Invalid Input: logged for inspection");
         db.close();
       });
     });
